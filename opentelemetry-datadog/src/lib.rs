8399536c28e69456ee98870f4e2714deb980ad4d
Hartigan <hartigans@live.com>
2023-10-13 15:13:10 +0200

    Pass string value into trace state

--- opentelemetry-datadog/src/lib.rs
+++ opentelemetry-datadog/src/lib.rs
@@ -274,7 +274,7 @@ mod propagator {
             let trace_state = TraceState::default();
             #[cfg(feature = "agent-sampling")]
             let trace_state = TraceState::from_key_value(
-                [(TRACE_STATE_PRIORITY_SAMPLING, sampled.is_sampled())]
+                [(TRACE_STATE_PRIORITY_SAMPLING, if sampled.is_sampled() { "1" } else { "0" })]
             ).unwrap_or_default();
 
             Ok(SpanContext::new(
